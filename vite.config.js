0a405a350ec76bb4efc518c8056ddf1f0faa1542
rx7-fc3s <1269804557@qq.com>
2023-08-04 13:43:58 +0800

    Upload:upload base to './

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: './',
+  plugins: [vue()],
   server: {
-    base: '/WMS-FRONTEND/',
-    cors: false,
     proxy: {
       '^/api': {
         target: 'http://127.0.0.1:8000/',
